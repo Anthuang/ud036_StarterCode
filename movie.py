82d231a13956f24b003ede7df75bc58b2535f5c5
Anthony Huang <anthuang@umich.edu>
2017-04-06 12:46:09 -0400

    Made all the suggested changes. Changed the style in run.py, and added documentation in movie.py.

--- movie.py
+++ movie.py
@@ -1,4 +1,8 @@
 class Movie(object):
+    """
+    Movie object that holds a title, image url, and youtube url
+    The constructor takes those respective arguments
+    """
     def __init__(self, title, image_url, youtube_url):
         self.title = title
         self.poster_image_url = image_url
--- run.py
+++ run.py
@@ -2,9 +2,15 @@ import fresh_tomatoes
 from movies import Movie
 
 # Creating Movie objects
-movie1 = Movie("Get Out", "http://www.trbimg.com/img-57f4386d/turbine/la-et-hc-get-out-horror-peele-20161004-snap", "https://www.youtube.com/watch?v=A2JbO9lnVLE")
-movie2 = Movie("Logan", "http://t1.gstatic.com/images?q=tbn:ANd9GcRPoMqL1vglrh7OF_69pT8gYMYnYaq1r7WfPMcD587V9uOR_hW2", "https://www.youtube.com/watch?v=Div0iP65aZo")
-movie3 = Movie("Days of Future Past", "http://t3.gstatic.com/images?q=tbn:ANd9GcSp1xUUrh6IW4gwa8j7p8WxU7Yt21mWhLVwZ5CejaXF6KsrOtjs", "https://www.youtube.com/watch?v=pK2zYHWDZKo")
+movie1 = Movie("Get Out", "http://www.trbimg.com/img-57f4386d/turbine/" \
+                + "la-et-hc-get-out-horror-peele-20161004-snap",
+                "https://www.youtube.com/watch?v=A2JbO9lnVLE")
+movie2 = Movie("Logan", "http://t1.gstatic.com/images?q=tbn:ANd9GcRPo" \
+                + "MqL1vglrh7OF_69pT8gYMYnYaq1r7WfPMcD587V9uOR_hW2",
+                "https://www.youtube.com/watch?v=Div0iP65aZo")
+movie3 = Movie("Days of Future Past", "http://t3.gstatic.com/images?q=tbn:" \
+                + "ANd9GcSp1xUUrh6IW4gwa8j7p8WxU7Yt21mWhLVwZ5CejaXF6KsrOtjs",
+                "https://www.youtube.com/watch?v=pK2zYHWDZKo")
 
 # Create list of movies to feed into the open_movies_page function
 list = (movie1, movie2, movie3)
