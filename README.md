c2ea5c434a355429ef5c2335e999532597da985e
Anthony Huang <anthuang@umich.edu>
2017-04-05 23:40:32 -0400

    Done

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ud036_StarterCode
 Source code for a Movie Trailer website.
+
+# Setup
+Run the run.py python script
---
+++ movies.py
@@ -0,0 +1,5 @@
+class Movie(object):
+    def __init__(self, title, image_url, youtube_url):
+        self.title = title
+        self.poster_image_url = image_url
+        self.trailer_youtube_url = youtube_url
